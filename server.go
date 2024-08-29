cf8257a7d1a5767d347a47af4ac5ce80f3d611af
SamuelLago <Samuelsdlago@gmail.com>
2024-08-29 15:34:46 -0300

    Add files via upload

---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Task struct { //Estrutura para
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) { // ResponseWriter = Retorna dados para o usuario // http.Request = solicitacoes para a API
+		w.Write([]byte("Servidor funcionando!")) // Write = Retorna a situacao do servidor ao usuario // bytes = formato ideal
+	})
+
+	fmt.Println("Servidor rodando na porta 5555...")
+	http.ListenAndServe(":5555", nil) // 1- porta no localhost // 2- nulo
+}
